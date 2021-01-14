ed21616000bdb059ab17ccbbf8cfefb199ace3d1
Mas Proxy <afifalfauzan65@gmail.com>
2021-01-14 15:30:00 +0700

    update
    button up

--- action.js
+++ action.js
@@ -8,7 +8,20 @@ window.onload = function(){
     }, false); 
 }
 
+window.onscroll = function(){
+    mybutton = document.getElementById("btn_up");
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none"
+    }
+}
 function goTo(tujuan){
     var element = document.getElementById(tujuan);
     element.scrollIntoView();;
+}
+
+function goUp(){
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
