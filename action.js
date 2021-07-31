f040fc9fd65bbc3543d5b48b6e2fe670cebd7bc9
Mas Proxy <afifalfauzan65@gmail.com>
2021-07-31 20:41:21 +0700

    fix float button

--- action.js
+++ action.js
@@ -22,8 +22,10 @@ function goTo(tujuan){
 }
 
 function goUp(){
+    mybutton = document.getElementById("btn_up");
     document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    mybutton.style.color = "white";
 }
 
 function goURL(url){
